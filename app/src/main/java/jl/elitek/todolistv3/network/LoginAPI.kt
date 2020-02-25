e12d2b40b61d5c84002f74a2df833d9098aeaed2
José Luis Zarza <jose.zarza@gmail.com>
2020-02-25 10:09:45 -0300

    Initial commit: V1.0

---
+++ app/src/main/java/jl/elitek/todolistv3/network/LoginAPI.kt
@@ -0,0 +1,39 @@
+package jl.elitek.todolistplus2.network
+
+import android.util.Log
+import jl.elitek.todolistplus2.Params
+import jl.elitek.todolistplus2.models.Persona
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface LoginAPI {
+
+    @POST("persona/login/{user}/{pass}")
+    fun postLogin(
+        @Path("user") user: String,
+        @Path("pass") pass: String
+       /* @Query("api_key") apiKey: String,
+        @Query("language") language: String,
+        @Query("page") page: Int*/
+    ): Call<Persona>
+
+    companion object {
+        val BASE_URL = Params.BASE_URL//"https://534d3695.ngrok.io/tdlpp_rest/api/"
+
+
+        fun create(): LoginAPI {
+           // Log.d("REST","$BASE_URL")
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            return retrofit.create(LoginAPI::class.java)
+        }
+    }
+}
