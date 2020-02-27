079981e9cb65b80df08ac531da4e85bacc737701
José Luis Zarza <jose.zarza@gmail.com>
2020-02-27 03:31:11 -0300

    Avances hasta integración de nuevas funcionalidades:  Paso de parametros entre fragments por medio de ViewModels()  Menu principal y más destinos

--- app/src/main/java/jl/elitek/todolistv3/MainActivity.kt
+++ app/src/main/java/jl/elitek/todolistv3/MainActivity.kt
@@ -2,16 +2,28 @@ package jl.elitek.todolistv3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //NUEVO
+        setSupportActionBar(my_toolbar as Toolbar)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_hamburger_menu)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView.setNavigationItemSelectedListener(this)
+        //fin NUEVO
      //   setSupportActionBar(toolbar) no funciona, ya existe una actionBar por omisión
         setupNavigation()
     }
@@ -45,6 +57,21 @@ class MainActivity : AppCompatActivity() {
 
         // Tie nav graph to items in nav drawer
         NavigationUI.setupWithNavController(navigationView, navController)
+    }
+
+    override fun onNavigationItemSelected(menu: MenuItem): Boolean {
+        when(menu.itemId){
+            R.id.nuevo_proyecto_menu_item -> {
+//                cambiarFragment(Fragment1.newInstance())
+            }
+            R.id.nueva_tarea_menu_item-> {
+  //              cambiarFragment(Fragment2.newInstance())
+            }
+            R.id.nuevo_usuario_menu_item -> {
+//                cambiarFragment(Fragment3.newInstance())
+            }
+        }
+        return true
     }
 
 }
