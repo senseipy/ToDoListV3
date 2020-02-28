4d202a92a61121f6315a2f1725fe04a9bcadba11
José Luis Zarza <jose.zarza@gmail.com>
2020-02-28 11:20:58 -0300

    Avances hasta integración de nuevas funcionalidades:                                                               Se agrego la creación de proyectos. El server está offline, hay que probar con el server andando

--- app/src/main/java/jl/elitek/todolistv3/Params.kt
+++ app/src/main/java/jl/elitek/todolistv3/Params.kt
@@ -5,7 +5,7 @@ import jl.elitek.todolistv3.models.Persona
 
 class Params {
     companion object{
-        val BASE_URL = "https://ffb9f835.ngrok.io/tdlpp_rest/api/"
+        val BASE_URL = "https://fb8bdd76.ngrok.io/tdlpp_rest/api/"
         private lateinit var persona: Persona
 
         fun setPersona(pers:Persona){
